3664b79630d7c676450c65f8386fea8eebf502ea
nabmaster <neonab@hotmail.com>
2018-04-10 11:53:32 +0200

    Auto-generated commit

--- main.bundle.js
+++ main.bundle.js
@@ -191,13 +191,9 @@ var BookService = /** @class */ (function () {
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
 var environment = {
-    production: false,
-    apiUrl: '/assets/data/poc.json'
+    production: true,
+    apiUrl: '/evaluation-nabil/assets/data/poc.json'
 };
 
 
